f2c3944fe9228480f89b05e6f77ce7bdb216ab5c
Raphaeljsx <raphaelalvoliv@gmail.com>
2024-02-06 15:21:38 -0300

    Ajuste

--- src/Components/ProjectsGrid.jsx
+++ src/Components/ProjectsGrid.jsx
@@ -113,7 +113,7 @@ const ProjectsGrid = () => {
             >
 
             {projeto .status ? <button
-                className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium 
+                className="text-white inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium 
                 ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring 
                 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-primary text-primary-foreground 
                 hover:bg-primary/90 h-10 px-4 py-2 lg:opacity-0 group-hover:opacity-100 transition-opacity duration-200"
